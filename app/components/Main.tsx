9838bfdf7e0742d597dfbe13815fd5f2a850f353
Diego Carmona <diegoncarmona@gmail.com>
2025-07-15 21:47:21 -0300

    fix HTML typo

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,12 +1,20 @@
 import VantaBackground from "./VantaBackground";
-import { spaceGrotesk, outfit } from '../fonts'
+import { spaceGrotesk, outfit } from "../fonts";
 
 export default function Main() {
   return (
     <VantaBackground>
       <div className="flex flex-col justify-center text-white h-screen px-5">
-        <h1 className={`text-[40px] md:text-[100px] md:pl-50 font-bold ${outfit.className}`}>Hey, I'm Diego</h1>
-        <p className={`mt-4 md:text-[40px] md:pl-50 text-lg ${spaceGrotesk.className}`}>A Front-End Developer</p>
+        <h1
+          className={`text-[40px] md:text-[100px] md:pl-50 font-bold ${outfit.className}`}
+        >
+          Hey, I&apos;m Diego
+        </h1>
+        <p
+          className={`mt-4 md:text-[40px] md:pl-50 text-lg ${spaceGrotesk.className}`}
+        >
+          A Front-End Developer
+        </p>
       </div>
     </VantaBackground>
   );
